898e2ca4112eb2dc1837e6fc30feed11569f2b0b
Thibault Marchand <Thbault.Marchand@grenoble-inp.org>
2025-12-18 19:02:42 +0100

    Feat test pour hello avec gen code

--- src/test/java/fr/ensimag/deca/tree/ManualTestInitialGencode.java
+++ src/test/java/fr/ensimag/deca/tree/ManualTestInitialGencode.java
@@ -5,7 +5,16 @@
  */
 package fr.ensimag.deca.tree;
 
+import java.io.File;
+import java.io.IOException;
+
+import org.antlr.v4.runtime.CommonTokenStream;
+
+import fr.ensimag.deca.CompilerOptions;
 import fr.ensimag.deca.DecacCompiler;
+import fr.ensimag.deca.syntax.AbstractDecaLexer;
+import fr.ensimag.deca.syntax.DecaLexer;
+import fr.ensimag.deca.syntax.DecaParser;
 
 /**
  *
@@ -27,12 +36,40 @@ public class ManualTestInitialGencode {
         lexp2.add(new StringLiteral("everybody !"));
         return source;
     }
+    public static AbstractProgram initTest2(String[] args) throws IOException{
+        DecaLexer lex = AbstractDecaLexer.createLexerFromArgs(args);
+        CommonTokenStream tokens = new CommonTokenStream(lex);
+        DecaParser parser = new DecaParser(tokens);
+        File file = null;
+        if (lex.getSourceName() != null) {
+            file = new File(lex.getSourceName());
+        }
+        final DecacCompiler decacCompiler = new DecacCompiler(new CompilerOptions(), file);
+        parser.setDecacCompiler(decacCompiler);
+        AbstractProgram source = parser.parseProgramAndManageErrors(System.err);
+        return source;
+    }
     
     public static String gencodeSource(AbstractProgram source) {
         DecacCompiler compiler = new DecacCompiler(null,null);
         source.codeGenProgram(compiler);
         return compiler.displayIMAProgram();
     }
+
+    public static void test2(String[] args) throws IOException {
+        AbstractProgram source = initTest2(args);
+        System.out.println("---- From the following Abstract Syntax Tree ----");
+        source.prettyPrint(System.out);
+        System.out.println("---- We generate the following assembly code ----");        
+        String result = gencodeSource(source);
+        System.out.println(result);
+        assert(result.equals(
+                "; Main program\n" +
+                "; Beginning of main function:\n" +
+                "	WSTR \"Hello\"\n" +
+                "	WNL\n" +
+                "	HALT\n"));
+    }    
 
     public static void test1() {
         AbstractProgram source = initTest1();
@@ -48,11 +85,10 @@ public class ManualTestInitialGencode {
                 "	WSTR \"everybody !\"\n" +
                 "	WNL\n" +
                 "	HALT\n"));
-    }    
-
+    }
         
-        
-    public static void main(String args[]) {
+    public static void main(String args[]) throws IOException {
         test1();
+        test2( new String[] {"src/test/deca/syntax/valid/provided/hello.deca"});
     }
 }
