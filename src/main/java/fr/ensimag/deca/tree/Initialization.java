45119a7560144da98aaa99d1fd9285c381a40b5c
Thibault Marchand <Thbault.Marchand@grenoble-inp.org>
2026-01-12 00:00:46 +0100

    feat: decopile init

--- src/main/java/fr/ensimag/deca/tree/Initialization.java
+++ src/main/java/fr/ensimag/deca/tree/Initialization.java
@@ -49,7 +49,8 @@ public class Initialization extends AbstractInitialization {
 
     @Override
     public void decompile(IndentPrintStream s) {
-        throw new UnsupportedOperationException("Not yet implemented");
+        s.print(" = ");
+        getExpression().decompile(s);
     }
 
     @Override
