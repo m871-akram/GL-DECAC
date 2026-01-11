d38c8f6c937c40c53db047a29ece72084dda72e7
Ahmed Bennasser <ahmed.bennasser@grenoble-inp.org>
2026-01-11 22:54:13 +0000

    Modulo

--- src/main/java/fr/ensimag/deca/tree/Modulo.java
+++ src/main/java/fr/ensimag/deca/tree/Modulo.java
@@ -23,8 +23,21 @@ public class Modulo extends AbstractOpArith {
     @Override
     public Type verifyExpr(DecacCompiler compiler, EnvironmentExp localEnv,
             ClassDefinition currentClass) throws ContextualError {
-        throw new UnsupportedOperationException("not yet implemented");
+        //throw new UnsupportedOperationException("not yet implemented");
+        Type t1 = this.getLeftOperand().verifyExpr(compiler, localEnv, currentClass);
+        Type t2 = this.getRightOperand().verifyExpr(compiler, localEnv, currentClass);
+
+       // Le modulo nécessite deux entiers  et return un int
+        if (!t1.isInt() || !t2.isInt()) {
+            throw new ContextualError(
+                "Modulo nécessite deux entiers, pas " + t1 + " et " + t2,
+                this.getLocation());
+        }
+
+        setType(compiler.getEnvironmentType().INT);
+        return getType();
     }
+
 
     @Override
     protected Instruction getInstruction(GPRegister op1, GPRegister op2) {
