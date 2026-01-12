73b51c4918a606221e4870cbe7d5da8a6b0a5082
Thibault Marchand <Thbault.Marchand@grenoble-inp.org>
2026-01-12 10:33:54 +0100

    fix: float literal verify

--- src/main/java/fr/ensimag/deca/tree/FloatLiteral.java
+++ src/main/java/fr/ensimag/deca/tree/FloatLiteral.java
@@ -39,7 +39,8 @@ public class FloatLiteral extends AbstractExpr {
     @Override
     public Type verifyExpr(DecacCompiler compiler, EnvironmentExp localEnv,
             ClassDefinition currentClass) throws ContextualError {
-        throw new UnsupportedOperationException("not yet implemented");        
+        setType(compiler.environmentType.FLOAT);
+        return getType();
     }
 
     @Override
