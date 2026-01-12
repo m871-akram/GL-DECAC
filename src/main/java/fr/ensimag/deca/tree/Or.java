474496d4f697efc54b9eb9183ce13efa4cfefbca
Mohammed Lrhorfi <mohammed.lrhorfi@grenoble-inp.org>
2026-01-12 09:38:18 +0100

    partie c , sans objet , sans if et while

--- src/main/java/fr/ensimag/deca/tree/Or.java
+++ src/main/java/fr/ensimag/deca/tree/Or.java
@@ -1,4 +1,6 @@
 package fr.ensimag.deca.tree;
+import fr.ensimag.deca.DecacCompiler;
+import fr.ensimag.ima.pseudocode.Label;
 
 
 /**
@@ -18,4 +20,42 @@ public class Or extends AbstractOpBool {
     }
 
 
+
+   // compteur  pour générer des labels uniques 
+    private static int count = 0;
+
+    @Override
+    protected void codeGenBool(DecacCompiler compiler, boolean branchOn, Label target) {
+        
+        if (branchOn) {
+            // sauter si (A || B) est Vrai
+            
+            
+            getLeftOperand().codeGenBool(compiler, true, target);
+            getRightOperand().codeGenBool(compiler, true, target);
+            
+        } else {
+            // sauter si (A || B) est Faux
+        
+            count++;
+            Label endOrLabel = new Label("end_or." + count);
+            
+            // Si A est Vrai, on saute à la fin 
+            getLeftOperand().codeGenBool(compiler, true, endOrLabel);
+            // Si B est Faux, alors tout est Faux -> on saute à target.
+            getRightOperand().codeGenBool(compiler, false, target);
+            
+            compiler.addLabel(endOrLabel);
+        }
+
+        
+    }
+
+
+
+
+
+
 }
+
+
